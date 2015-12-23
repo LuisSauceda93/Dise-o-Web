72636c1332a5a22d539944897efb312ff50bd4ee
LuisSauceda93 <luigalberto93@hotmail.com>
2015-12-22 23:08:24 -0600

    Se agrego la funcionalidad del boton publicar en index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-sweetAlert("Hola Sauceda");
+var $form = $("#formulario"),
+    $titulo = $("#titulo"),
+    $url = $("#url"),
+    $button = $("#mostrar-form"),
+    $list = $("#contenido"),
+    $post = $(".item").first();
+
+//Eventos
+
+function mostrarFormulario() {
+  // body...
+  $form.slideToggle();
+  return false;
+}
+
+function agregarPost() {
+  // body...
+  var url = $url.val(),
+      titulo = $titulo.val();
+      $clone = $post.clone();
+  $clone.find(".titulo_item a").text(titulo).attr("href", url);
+
+  $clone.hide();
+  $list.prepend($clone);
+  $clone.fadeIn();
+  $titulo.val("");
+  $url.val("");
+
+  return false;
+}
+
+$button.click(mostrarFormulario);
+$form.on("submit", agregarPost);
